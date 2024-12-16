4995466da2f2a46868ee925774309ac6495abb66
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-12-16 09:56:24 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ const App = () => {
       <Header />
       <Hero />
       <About />
-      <Mics />
-      <Map />
+      {" <Mics /> "}
+      { " <Map /> " }
       <Footer />
     </div>
   );
